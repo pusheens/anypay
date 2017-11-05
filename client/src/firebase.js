a047616a10e3a802ef478d52b4c9bde4d5a3bf37
nickbreaton <nick@breaton.com>
2017-11-05 01:44:10 -0400

    Add login

---
+++ client/src/firebase.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase'
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyAB4SleYvI3mNRw7qWVPcKnZUWTZv4eGo4',
+  authDomain: 'anypay-e40b4.firebaseapp.com',
+  databaseURL: 'https://anypay-e40b4.firebaseio.com/'
+})
