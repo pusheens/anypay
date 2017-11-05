f91b3c9c86eb24f10ee9515a3eaacf7c1b0178ce
Elizabeth Thompson <ethomp54@uncc.edu>
2017-11-05 07:56:39 -0500

    home

--- client/src/containers/AddBankAccount.js
+++ client/src/containers/AddBankAccount.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
+import axios from 'axios'
+import firebase from 'firebase'
+
 import Button from '../components/Button'
 import Input from '../components/Input'
 
 export default class AddBankAccount extends React.Component {
+  addBank = async event => {
+    event.preventDefault()
+
+    try {
+      const email = firebase.auth().currentUser.email
+
+      const { data } = await axios.post('http://localhost:3000/has_bank', {
+        email,
+        has_bank: true
+      })
+
+      this.props.history.push('/home')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   render () {
     return (
       <div className='container'>
@@ -16,7 +36,7 @@ export default class AddBankAccount extends React.Component {
           <Input id='accountNumber' label='Account Number' type='text' />
         </div>
         <div className='flex-end is-centered'>
-          <Button to='/signup6' text='Add Bank Account' type='gradient' />
+          <Button to='' text='Add Bank Account' type='gradient' />
         </div>
       </div>
     )
